4cd8ec6f40d54aecd921ae0327f93452163906f3
M J Harvey <moonbug1970@gmail.com>
2016-08-25 15:10:15 +0100

    templates and zipping output

--- src/deploy.py
+++ src/deploy.py
@@ -4,6 +4,9 @@ from __future__ import print_function
 
 import os
 import re
+import zipfile
+import glob
+
 # Get slot dir
 cwd = os.getcwd()
 
@@ -17,12 +20,16 @@ install_dir = os.path.join( home, ".gpugrid", "slot-" + slot )
 if not os.path.exists( install_dir ):
   os.makedirs( install_dir )
 
-conda  = os.path.join( install_dir, "bin", "conda" )
+conda   = os.path.join( install_dir, "bin", "conda" )
+python  = os.path.join( install_dir, "bin", "python" )
 
-if not os.path.exists( conda ):
+if not os.path.exists( conda ) or not os.path.exists(python):
   print("Installing Miniconda to " + install_dir );
   installer = os.path.join( cwd, "miniconda-installer" )
   os.system( installer + " -b -f -p " + install_dir )
+# TODO:
+# pre-emptively remove any conda lock files
+# eg /home/x/.gpugrid/slot-0/pkgs/.conda_lock-21591
 
 # configure channels"
 os.system( conda + " config --add channels omnia" )
@@ -32,9 +39,21 @@ if os.path.exists("dependencies.txt"):
   f = open( "dependencies.txt", "r" )
   lines = f.readlines()
   for dep in lines:
+    dep = dep.strip()
     print("Installing dependency " + dep);
     os.system( conda + " install " + dep + " -y" );
 
 print("Updating distro")
 os.system( conda + " update --all -y" )
 
+if os.path.exists("payload.py"):
+  print("Running payload")
+  os.system( python + " payload.py" )
+
+if os.path.exists("output"):
+  print("Creating output archive");
+  zf = zipfile.ZipFile("output.zip", "w" )
+  for f in os.listdir("output"):
+    print("  Adding " + f );
+    zf.write( os.path.join("output", f), arcname=f )
+  zf.close()
